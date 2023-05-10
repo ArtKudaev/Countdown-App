8fcbc5f113230185889afe466a9d48ba6be20f5a
ArtKudaev <oldoldskoolgamer@gmail.com>
2023-05-10 15:54:24 +0300

    Changed the way the app works

--- datasearch.js
+++ datasearch.js
@@ -1,5 +1,5 @@
 
-import { differenceInYears, differenceInDays, differenceInHours, parseISO } from 'date-fns'; 
+/* import { differenceInYears, differenceInDays, differenceInHours, parseISO } from 'date-fns'; 
 const input = document.querySelector('.input');
 const btnRequest = document.querySelector('.button');
 
@@ -19,4 +19,41 @@ btnRequest.addEventListener('click', e => {
     const hoursResult = differenceInHours(date, currentDate);
     document.querySelector('.output-hours').innerHTML = `Часов: ${hoursResult}`;
     }
+); */
+
+import { intervalToDuration, parseISO } from 'date-fns'
+const input = document.querySelector('.input');
+const btnRequest = document.querySelector('.button');
+btnRequest.addEventListener('click', e => {
+    e.preventDefault();
+    let currentDate = new Date();
+    let date = parseISO(input.value);
+    console.log(input.value);
+    console.log(currentDate);
+
+    let duration = intervalToDuration({
+        start: currentDate, 
+        end: date,
+    })
+    console.log(duration);
+        
+    const yearsResult = duration.years;
+    document.querySelector('.output-years').innerHTML = `Лет: ${yearsResult}`;
+
+    const daysResult = duration.days;
+    document.querySelector('.output-days').innerHTML = `Дней: ${daysResult}`;
+
+    const hoursResult = duration.hours;
+    document.querySelector('.output-hours').innerHTML = `Часов: ${hoursResult}`;
+    }
 );
+
+
+/* let duration = intervalToDuration({
+    start: new Date(2022, 6, 2, 0, 0, 15), 
+    end: new Date(),
+})
+
+formatDuration(duration, {
+    delimiter: ', '
+}) */
